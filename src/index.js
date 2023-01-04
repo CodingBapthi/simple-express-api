8cc73c8af4d6168d9a468c46df72d24295546b39
s.tittel <s.tittel@ecentral.de>
2023-01-04 15:57:16 +0100

    Add CORS support to Express app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 require('dotenv').config();
+
+// Cors middleware to allow cross-origin requests
+// When you limit the domains that are allowed to access your API, you can use the cors middleware to allow cross-origin requests.
+// app.use(cors({ origin: ['http://example.com', 'http://my-app.com'] }));
+app.use(cors());
 
 // Port variable
 const PORT = process.env.PORT || 3001;
